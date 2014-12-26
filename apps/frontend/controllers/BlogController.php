3708f68d84cb2239533e4ab4896c068e401a1b4a
hongker <xiaok2013@live.com>
2014-12-26 10:02:36 +0800

    设计博客模块

---
+++ apps/frontend/controllers/BlogController.php
@@ -0,0 +1,23 @@
+<?php
+namespace Frontend\Controllers;
+
+/**
+ * 简述：博客模块。
+ * 
+ * <code>BlogController<code/> 继承自 <code>BaseController</code> 类
+ * 
+ * @author hongker
+ * @version 1.0
+ * @copyright Powerd by hongker
+ * @package Frontend\Controllers
+ *
+ */
+class BlogController extends BaseController {
+	/**
+	 * 首页
+	 */
+	public function indexAction() {
+		
+	}
+	
+}
