75a256931cb09e8b7ea981d646885105f74743fa
hongker <xiaok2013@live.com>
2014-12-29 14:35:23 +0800

    添加关于我们模块

---
+++ apps/frontend/controllers/AboutController.php
@@ -0,0 +1,26 @@
+<?php
+namespace Frontend\Controllers;
+
+use Models\Articles;
+/**
+ * 简述：关于我们。
+ * 
+ * <code>AboutController<code/> 继承自 <code>BaseController</code> 类
+ * 
+ * @author hongker
+ * @version 1.0
+ * @copyright Powerd by hongker
+ * @package Frontend\Controllers
+ *
+ */
+class AboutController extends BaseController {
+	/**
+	 * 首页
+	 */
+	public function indexAction() {
+		
+	}
+	
+	
+	
+}
